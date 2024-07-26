208dcfaaea7a8e34184f2bee7c6fae7b118cd6c3
onursapan <onursapan@hotmail.com>
2024-07-26 23:39:12 +0300

    Initial Application Setup
    Set up the foundation for the application, including project structure, initial configurations, and necessary dependencies.

---
+++ AppStoreCase/AppDelegates/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  AppStoreCase
+//
+//  Created by Onur Sapan on 26.07.2024.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
